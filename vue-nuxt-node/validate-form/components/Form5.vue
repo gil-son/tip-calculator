72bf0f0ee5819baf02e78402a84d2a59e9518c24
gilszon <gilszon@gmail.com>
2022-02-09 17:34:04 -0300

    Form 5

--- vue-nuxt-node/validate-form/components/Form5.vue
+++ vue-nuxt-node/validate-form/components/Form5.vue
@@ -27,6 +27,7 @@
         label="Message"
         rows="1"
         counter="250"
+        id="idDoElemento"
       ></v-textarea>
       
       
@@ -41,7 +42,6 @@
         Clear
       </v-btn>
       <v-btn
-        id="idDoElemento"
         text
         @click="format()"
       >
@@ -59,52 +59,23 @@
         Submit
       </v-btn>
     </v-card-actions>
-    <v-dialog
-      v-model="dialog"
-      absolute
-      max-width="400"
-      persistent
-    >
-      <v-card>
-        <v-card-title class="text-h5 grey lighten-3">
-          Legal
-        </v-card-title>
-        <v-card-text>
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
-        </v-card-text>
-        <v-divider></v-divider>
-        <v-card-actions>
-          <v-btn
-            text
-            @click="agreement = false, dialog = false"
-          >
-            No
-          </v-btn>
-          <v-spacer></v-spacer>
-          <v-btn
-            class="white--text"
-            color="deep-purple accent-4"
-            @click="agreement = true, dialog = false"
-          >
-            Yes
-          </v-btn>
-        </v-card-actions>
-      </v-card>
-    </v-dialog>
+    <div>
+      <p><b>{{message}}</b></p>
+    </div>
   </v-card>
+  
 </template>
 
 <script>
   export default {
     data: () => ({
+      message:'',
       agreement: false,
       
     }),
     methods: {
       format () {
-        let el = document.getElementById("idDoElemento");
-        console.log("Test: ",el.innerHTML.bold);
-
+        
       }
     }
   }
